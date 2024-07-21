7686de655f7821db42e9d614380a181bb9739f77
Samer Ibrahimovic <sameribrahimovic@hotmail.com>
2024-07-21 15:20:41 +0200

    base layout and auth added. Theme switcher added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello Reminder App</h1>;
+  return <h1>Hello from dashboard</h1>;
 }
