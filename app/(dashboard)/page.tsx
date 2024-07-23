08cea135c0c2b3073ec65e0c9b35755cf5ba56b5
Samer Ibrahimovic <sameribrahimovic@hotmail.com>
2024-07-23 12:17:25 +0200

    collection card added and ui finished. Modal to delete collection added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,10 @@
-import CollectionList from "@/components/CollectionList";
+import CollectionCard from "@/components/CollectionCard";
+import CreateCollectionButton from "@/components/CreateCollectionButton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
+import { Frown } from "lucide-react";
 import { Suspense } from "react";
 
 export default async function Home() {
@@ -40,5 +44,38 @@ function WelcomeMsgFallback() {
         <Skeleton className="w-[250px] h-[36px]" />
       </h1>
     </div>
+  );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: { userId: user?.id },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <Frown />
+          <AlertTitle>There are no collections created yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started!
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionButton />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <CreateCollectionButton />
+      <div className="flex flex-col gap-4 mt-4">
+        {collections.map((collection) => (
+          <CollectionCard key={collection.id} collection={collection} />
+        ))}
+      </div>
+    </>
   );
 }
---
+++ components/CollectionCard.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { Collection } from "@prisma/client";
+import React, { useState } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { CollectionColor, CollectionColors } from "@/lib/constants";
+import { CaretDownIcon, CaretUpIcon } from "@radix-ui/react-icons";
+import { Progress } from "./ui/progress";
+import { Separator } from "./ui/separator";
+import { Plus, Trash } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+
+interface Props {
+  collection: Collection;
+}
+
+//for testing until real data form db comes
+const tasks: string[] = ["Task 1", "Task 2"];
+
+function CollectionCard({ collection }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+      {/* asChild prevents hidration error */}
+      <CollapsibleTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className={cn(
+            "w-full flex justify-between p-6",
+            isOpen && "rounded-b-none",
+            CollectionColors[collection.color as CollectionColor]
+          )}
+        >
+          <span className="text-white font-bold">{collection.name}</span>
+          {!isOpen && <CaretDownIcon className="h-6 w-6" />}
+          {isOpen && <CaretUpIcon />}
+        </Button>
+      </CollapsibleTrigger>
+      <CollapsibleContent className="flex rounded-b-md flex-col">
+        {tasks.length === 0 && <div>No tasks</div>}
+        {tasks.length > 0 && (
+          <>
+            <Progress className="rounded-none" value={45} />
+            <div className="p-4 gap-3 flex flex-col">
+              {tasks.map((task) => (
+                <div>Mocked taks</div>
+              ))}
+            </div>
+          </>
+        )}
+        <Separator />
+        <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center">
+          {/* here I use doDateString() insted of toLoaceDateStrig() to avoid hidration error */}
+          Created at {collection.createdAt.toDateString()}
+          <div>
+            <Button variant={"ghost"} size={"icon"}>
+              <Plus />
+            </Button>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant={"ghost"} size={"icon"}>
+                  <Trash />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogTitle>Are you sure ?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone! This will permanently delete
+                  your collection and all related tasks.
+                </AlertDialogDescription>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction>Delete</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        </footer>
+      </CollapsibleContent>
+    </Collapsible>
+  );
+}
+
+export default CollectionCard;
